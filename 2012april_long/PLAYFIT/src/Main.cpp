7e665016d18a9d160a58cfd8907872a97dcdc8a6
Alexander Dolidze <alexander.dolidze@gmail.com>
2012-04-10 01:47:29 +0400

    correct version

--- 2012april_long/PLAYFIT/src/Main.cpp
+++ 2012april_long/PLAYFIT/src/Main.cpp
@@ -1,21 +1,23 @@
- #include <cstdio>
- using namespace std;
+#include <cstdio>
+using namespace std;
+ 
+int main()
+{
+    int T=0;
+    scanf("%d\n", &T);
+    int N=0;
+    int current_value=0;
+    int min=0, max=0, gl_max=0;
 
- int main()
- {
-
-     int T;
-     scanf("%d\n", &T);
-     int N;
-     int current_value;
-     int min, max, gl_max;
-
-     while(T>0) {
+    while(T>0) {
         T--;
+        gl_max=0;
 
         scanf("%d\n", &N);
         scanf("%d", &min);
+
         max = min;
+        N--;
         while (N>0) {
             N--;
             scanf(" %d", &current_value);
@@ -25,12 +27,16 @@
             }
             if (current_value > max) {
                 max = current_value;
-                gl_max = (gl_max < max) ? max : gl_max;
+                gl_max = (gl_max < (max-min)) ? (max-min) : gl_max;
             }
         }
-        scanf("\n");
-        if (gl_max > 0) printf("%d\n", gl_max); else printf("UNFIT\n");
-     }
 
+        if (gl_max > 0) {
+            printf("%d\n", gl_max);
+        } else {
+            printf("UNFIT\n");
+        }
+    }
+ 
     return 0;
- }
+}
